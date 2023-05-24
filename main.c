c273aa1d0ea86cd4ff880204b507e52aa2dad0e9
Batınay Ünsel <batinayunsel@gmail.com>
2023-05-25 00:26:25 +0300

    create create_task func

--- main.c
+++ main.c
@@ -111,3 +111,77 @@ void *task_fn(void *arg) {
 
     pthread_exit(NULL);
 }
+
+
+
+
+void create_task(int priority, int *resourcesnumber, double task_time) {
+    pthread_mutex_lock(&task_mutex);
+
+    // check if task number is max
+    if (taskNumber >= MAX_TASKS) {
+        printf("Maximum number of tasks reached! \n");
+        printFile(file_name, "Maximum number of tasks reached! \n");
+        pthread_mutex_unlock(&task_mutex);
+        return;
+    }
+
+	
+    int task_id = taskNumber++;
+	
+    tasks[task_id].id = task_id;
+    tasks[task_id].priority = priority;
+    tasks[task_id].state = 0; // Ready state
+    tasks[task_id].resources = malloc(MAX_RESOURCES * sizeof(int));
+    tasks[task_id].task_time = task_time;
+    tasks[task_id].text = selectRandomText();
+
+    // Copy resourcesnumber array to tasks[task_id].resources
+    int i;
+    for (i = 0; i < MAX_RESOURCES; i++) {
+        tasks[task_id].resources[i] = resourcesnumber[i];
+    }
+
+    pthread_mutex_unlock(&task_mutex);
+
+    // Acquire required resources
+    pthread_mutex_lock(&resource_mutex);
+
+    int can_be_reserved = 1; // flag for allocation
+    for (i = 0; i < MAX_RESOURCES; i++) {
+        if (resources.freeResources[i] < resourcesnumber[i]) {
+            can_be_reserved = 0;
+            printf("Not enough resources for this task ");
+            printf("Exiting...");
+            printFile(file_name, "Not enough resources for this task ");
+            printFile(file_name, "Exiting...");
+            sleep(3);
+            break;
+        }
+    }
+
+    if (can_be_reserved) {
+        // Allocate resources
+        for (i = 0; i < MAX_RESOURCES; i++) {
+            resources.freeResources[i] -= resourcesnumber[i];
+            resources.reserved[task_id][i] += resourcesnumber[i];
+        }
+        printf("Resources reserved for task %d \n", tasks[task_id].id);
+        printFile(file_name, "Resources reserved for task %d \n", task_id);
+        pthread_mutex_unlock(&resource_mutex);
+        sleep(2);
+
+        if (pthread_create(&tasks[task_id].thread, NULL, task_fn, (void *)&tasks[task_id]) != 0) {
+            printf("Error creating thread for task %d.\n", task_id);
+            printFile(file_name, "Error creating thread for task %d.\n", task_id);
+            return;
+        }
+    } else {
+        pthread_mutex_unlock(&resource_mutex);
+        printf("Not enough resources to allocate. Task %d is blocked.\n", task_id);
+        printFile(file_name, "Not enough resources to allocate. Task %d is blocked.\n", task_id);
+        return;
+    }
+
+    sem_post(&task_creation_semaphore);
+}
